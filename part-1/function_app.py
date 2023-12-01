6a0bcf64511b48515e4dc471d8c6d4d28b9ad346
Vuillemot Florian <vuillemot.florian@outlook.fr>
2023-12-01 06:04:24 +0000

    Fix function app

--- part-1/function_app.py
+++ part-1/function_app.py
@@ -31,4 +31,4 @@ def predict():
 
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    return func.WsgiMiddleware(app).handle(req, context)
+    return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
