454cfe4e904433b9f32d97e8df444e38398c985b
Vuillemot Florian <vuillemot.florian@outlook.fr>
2023-12-02 20:58:02 +0100

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -4,7 +4,7 @@ import azure.functions as func
 from sklearn.ensemble import RandomForestClassifier
 
 # Create the Azure Function App.
-# The level ANONYMOUS indicate there is no authentification need to access this API.
+# The ANONYMOUS level indicates no authentification is needed to access this API.
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 
@@ -12,25 +12,24 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 # This route listens `/predict`.
 @app.route(route="predict")
 def predict(req: func.HttpRequest):
-    # Retrieving the data to predict.
+    # Retrieve the data to be predicted.
     # For education purposes, there is no input validation.
     to_predict = req.get_json()
 
-    # Defining the model.
+    # Define the model.
     clf = RandomForestClassifier(random_state=0)
 
-    # Defining the training dataset.
-    X = [[ 1,  2,  3],
-        [11, 12, 13]]
+    # Define the training dataset.
+    X = [[ 1,  2,  3], [11, 12, 13]]
     y = [0, 1]
 
-    # Training the model on the dataset.
+    # Train the model on the dataset.
     clf.fit(X, y)
 
-    # Do the prediction.
+    # Run the prediction.
     prediction = clf.predict(to_predict)
 
-    # Converting the prediction for HTTP output.
+    # Convert the prediction for HTTP output.
     res = json.dumps(prediction.tolist())
 
     # Return the HTTP result.
