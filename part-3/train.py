46b30f2c26caae90550b49512ebf79e754277214
Vuillemot Florian <florian.vuillemot@avaiga.com>
2023-12-08 08:13:13 +0100

    update nb features

--- part-3/train.py
+++ part-3/train.py
@@ -12,7 +12,7 @@ clf = RandomForestClassifier(random_state=0)
 
 # Define the training dataset.
 X, y = make_classification(
-    n_samples=1000, n_features=2,
+    n_samples=1000, n_features=3,
     n_informative=2, n_redundant=0,
     random_state=0, shuffle=False
 )
