311d225f3faee52f4802c346c4d08a03cd6e90a8
Vuillemot Florian <florian.vuillemot@avaiga.com>
2023-12-08 07:48:04 +0100

    create fake dataset

--- part-3/train.py
+++ part-3/train.py
@@ -4,14 +4,18 @@ This script defines a model, trains it on a dataset, and saves the model to a fi
 import pickle
 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
 from sklearn.model_selection import cross_val_score
 
 # Define the model.
 clf = RandomForestClassifier(random_state=0)
 
 # Define the training dataset.
-X = [[ 1,  2,  3], [11, 12, 13]]
-y = [0, 1]
+X, y = make_classification(
+    n_samples=1000, n_features=2,
+    n_informative=2, n_redundant=0,
+    random_state=0, shuffle=False
+)
 
 # Train the model on the dataset.
 clf.fit(X, y)
@@ -21,5 +25,5 @@ with open('model.pkl', 'wb') as f:
     pickle.dump(clf, f)
 
 # Evaluate the model.
-scores = cross_val_score(clf, X, y, cv=1)
+scores = cross_val_score(clf, X, y, cv=5)
 print("%0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
